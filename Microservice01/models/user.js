7e247f12a40c496738d645f606ab5965c6d1a1de
Samtheopps <sami.ousmaal@supdevinci-edu.fr>
2025-11-19 14:25:41 +0100

    add user model with validation and schema definition

---
+++ Microservice01/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+require('mongoose-type-email');
+
+const userSchema = mongoose.Schema({
+        email: {
+            type: mongoose.SchemaTypes.Email,
+            required: [true, "l'email est obligatoire"],
+        },
+        username: {
+            type: String,
+            required: [true, "nom d'utilisateur vide ?"],
+            trim: true,
+            minLength: [3, "le nom d'utilisateur doit contenir au moins 3 caractère"],
+            maxLength: [15, "le nom d'utilisateur doit contenir au maximum 15 caractère"]
+        },
+        password: {
+            type: String,
+            required: [true, "mot de passe vide ?"]
+        },
+        role: {
+            type: String,
+            required: [true, "role est vide ?"]
+        },
+        reputation: {
+            type: Number,
+            min: [0, "une réputaion ne peut pas être négatif"],
+            max: [5, "une réputaion ne peut pas dépasser 5"]
+        }
+    },
+    {
+        virtuals: true,
+        timestamps: true,
+        toJSON: {
+            transform: function (doc, ret) {
+                delete ret.__v
+                return ret;
+            }
+        }
+    }
+);
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = User
+
+
